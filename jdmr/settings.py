cf1d8cd71a391fafeb74397ec841f7294c0f1872
Rajangupta09 <rjnkumar05@gmail.com>
2020-01-13 14:43:45 +0530

    added django-heroku

--- jdmr/settings.py
+++ jdmr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -133,3 +134,4 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'img')
 MEDIA_URL = '/img/'
 
+django_heroku.settings(locals())
