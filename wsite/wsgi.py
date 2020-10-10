59a3f7a1f9cade566d0442e012647fe72743e6e4
Rajangupta09 <rjnkumar05@gmail.com>
2020-10-10 11:42:07 +0530

    Optimizations

--- wsite/wsgi.py
+++ wsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for jdmr project.
+WSGI config for wsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jdmr.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wsite.settings')
 
 application = get_wsgi_application()
